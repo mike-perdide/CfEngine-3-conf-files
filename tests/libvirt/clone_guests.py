a6b0aeeeb892e6fe8c155bb305459951f39d067d
Julien Miotte <mike.perdide@gmail.com>
2012-03-10 18:50:03 +0100

    Renaming the id_clone_hosts script to clone_guests.
    Also, we're removing common methods since they can be imported from
    common.py, and we're moving code that is not strictly cloning to other
    scripts (watch for the following commits).

--- tests/libvirt/clone_guests.py
+++ tests/libvirt/clone_guests.py
@@ -4,7 +4,7 @@ SYNOPSIS
     id_clone_hosts.py config.cfg
 DESCRIPTION
     The config file given in parameter should specify the IP of the libvirt
-    host and the name of the virtual machines that must be cloned in the 
+    host and the name of the virtual machines that must be cloned in the
     following fashion:
 
         [virt_host]
@@ -15,7 +15,7 @@ DESCRIPTION
             clone_name=cfengine-hub
             hostname=cfhub
             ip=192.168.1.1
-        
+
         [MASTER-cfengine-client]
             clone_name=cfengine-client
             hostname=cfclient
@@ -25,54 +25,18 @@ DESCRIPTION
     purposes. The cloned machine will have the same hardware configuration as
     the original machine.
 
-    The network is defined by a libvirt compatible xml file (see libvirt
-    documentation for more specs). The xml declaration must contain an empty
-    <dhcp></dhcp> declaration, it will be used to assign IP addresses and
-    hostnames to the clones.
-
     To avoid having two identifical machines running at the same time, the
     program will stop if the original machine is running."""
 
 import sys
-import libvirt
-import ConfigParser
 from xml.etree import ElementTree
 from virtinst.CloneManager import CloneDesign, start_duplicate, \
                                   generate_clone_disk_path
-
-
-def parse_cfg(filepath):
-    config = ConfigParser.ConfigParser()
-    config.read(filepath)
-
-    virt_host = config.get("virt_host", "server")
-
-    conn = libvirt.open("qemu+ssh://%s/system" % virt_host)
-    if conn == None:
-        print "Failed to open connection to the hypervisor"
-        sys.exit(1)
-
-    machines_to_clone = set(config.sections()) - set(["virt_host"])
-    available_machines = conn.listDefinedDomains()
-
-    for machine in machines_to_clone:
-        if machine not in available_machines:
-            print machine, "is not amongst the avalaible virtual machines."
-            print "Please check below:"
-            print available_machines
-
-            sys.exit(2)
-
-        if conn.lookupByName(machine).isActive():
-            print machine, "is running. Aborting to avoid mac collision."
-            sys.exit(3)
-
-    return conn, machines_to_clone, config
+from common import parse_cfg
 
 
 def clone(conn, guest_name, cfg):
-    clone_mgr = CloneDesign(conn=conn)
-    print "guest_name", guest_name
+    clone_mgr = CloneDesign(conn)
     clone_mgr.original_guest = guest_name
     clone_mgr.setup_original()
 
@@ -97,11 +61,10 @@ def first_mac_address(conn, clone_name):
     return mac_element.get("address")
 
 
-def setup_network(conn, machines_to_clone):
+def setup_network(conn, xmlfilepath, cfg, machines_to_clone):
     mac_addresses = [(orig, first_mac_address(conn,
                                               cfg.get(orig, "clone_name")))
                      for orig in machines_to_clone]
-    xmlfilepath = cfg.get("virt_host", "network_definition")
 
     tree = ElementTree.ElementTree()
     tree.parse(xmlfilepath)
@@ -110,9 +73,9 @@ def setup_network(conn, machines_to_clone):
     for orig_name, mac in mac_addresses:
         hostname = cfg.get(orig_name, "hostname")
         ip = cfg.get(orig_name, "ip")
-        element = ElementTree.Element("host", { "mac": mac,
-                                                "name": hostname,
-                                                "ip": ip})
+        element = ElementTree.Element("host", {"mac": mac,
+                                               "name": hostname,
+                                               "ip": ip})
         dhcp_section.insert(0, element)
 
     netxml = ElementTree.tostring(tree.getroot())
@@ -124,17 +87,9 @@ if __name__ == "__main__":
         print USAGE
         sys.exit(1)
 
-    conn, machines_to_clone, cfg = parse_cfg(sys.argv[1])
+    configfilepath = sys.argv[1]
+    conn, machines_to_clone, cfg, xmlfilepath = parse_cfg(configfilepath)
 
     # Clone the machines
     for machine in machines_to_clone:
         clone(conn, machine, cfg)
-
-    # Setup the dhcp parameters
-    setup_network(conn, machines_to_clone)
-
-    # Start the cloned machines
-    for orig in machines_to_clone:
-        clone_name = cfg.get(orig, "clone_name")
-        clone_guest = conn.lookupByName(clone_name)
-        clone_guest.create()
